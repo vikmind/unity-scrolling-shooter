9d5b7ef8a6925239cfc56d59e51134001ee87ddb
Victor Marchenko <viktor.marchenko@funcfunc.ru>
2014-05-22 23:21:51 +0400

    Player and enemy

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+
+	private float inputX;
+	private float inputY;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		inputX = Input.GetAxis("Horizontal");
+		inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY
+		);
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+}
