27d083bce855d76777c1c3bce4e1b81c00b4a855
Victor Marchenko <viktor.marchenko@funcfunc.ru>
2014-05-26 23:11:53 +0400

    Shooting 1

---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour {
+
+	/// <summary>
+	/// Damage inflicted
+	/// </summary>
+	public int damage = 1;
+	
+	/// <summary>
+	/// Projectile damage player or enemies?
+	/// </summary>
+	public bool isEnemyShot = false;
+	
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		Destroy(gameObject, 20); // 20sec
+	}
+}
